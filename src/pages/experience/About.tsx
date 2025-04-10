ab5bd088c29521f7ad22f7f63124bee1b625c9da
jagadishck018 <jagadishck.018@gmail.com>
2025-04-10 14:43:16 +0530

    refactor About component layout for improved padding and alignment

--- src/pages/experience/About.tsx
+++ src/pages/experience/About.tsx
@@ -1,12 +1,12 @@
 const About = () => {
   return (
-    <div className="w-full  flex flex-col md:flex-row items-center md:items-start justify-center gap-7 max-w-screen-md mx-auto border-2 border-amber-50 ">
+    <div className="w-full  flex flex-col md:flex-row items-center md:items-start justify-center gap-7 max-w-screen-md mx-auto border-2 border-amber-50 px-2 ">
       <div>
         <h1 className="text-white md:w-35 md:sticky-10 font-medium text-start md:text-center">
           About
         </h1>
       </div>
-      <div className="text-white flex-wrap flex flex-col gap-2  text-start">
+      <div className="text-white flex-wrap flex flex-col gap-2  text-start ">
         <p>I'm Jagadish C K, a passionate Fullstack Developer</p>
         <p>
           with experience in building responsive web applications using modern
