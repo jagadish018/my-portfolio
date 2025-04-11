8c8723b5ba16e2b73761265cb28c4a1761acc11f
jagadishck018 <jagadishck.018@gmail.com>
2025-04-11 14:09:13 +0530

    feat: add Attributions component and update import path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Attributions from "./components/footers/pages/Attributions";
+import Attributions from "./components/footers/page/Attributions";
 
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
--- src/pages/interest/Interest.tsx
+++ src/pages/interest/Interest.tsx
@@ -1,5 +1,3 @@
-
-
 const Interest = () => {
   return (
     <div className="w-full  flex flex-col md:flex-row items-center md:items-start justify-center gap-7 max-w-screen-md mx-auto  px-4 ">
@@ -10,30 +8,30 @@ const Interest = () => {
       </div>
       <div className="text-white flex-wrap flex flex-col gap-4  text-start ">
         <p>
-          <strong>Web Development –   </strong>especially working with React,
+          <strong>Web Development – </strong>especially working with React,
           Tailwind CSS, and building full-stack apps using Hono, Node.js, and
           PostgreSQL.
         </p>
         <p>
-          <strong>Cybersecurity –   </strong>particularly fileless malware
+          <strong>Cybersecurity – </strong>particularly fileless malware
           detection using memory dumps and advanced analysis tools.
         </p>
         <p>
-          <strong>Cricket –   </strong> active participation in college-level
+          <strong>Cricket – </strong> active participation in college-level
           cricket, combining teamwork and strategic thinking.
         </p>
         <p>
-          <strong>UI/UX Design –   </strong>
+          <strong>UI/UX Design – </strong>
           designing clean, responsive, and user-friendly interfaces for your
           projects.
         </p>
         <p>
-          <strong> Tech Communities –   </strong> attending meetups like Synerz,
+          <strong> Tech Communities – </strong> attending meetups like Synerz,
           collaborating with peers, and staying updated with the latest trends.
         </p>
       </div>
     </div>
   );
-}
+};
 
-export default Interest
+export default Interest;
