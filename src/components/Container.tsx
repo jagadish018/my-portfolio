c36a7bf76de37d95838d5c02a964f9b65e91f8e5
jagadishck018 <jagadishck.018@gmail.com>
2025-04-11 09:00:54 +0530

    fix: remove border from Container component for cleaner appearance

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,6 @@
 const Container = ({ children }: { children?: React.ReactNode }) => {
   return (
-    <div className="max-w-screen-md mx-auto mt-16 px-4 p-3 relative custom-card rounded-2xl border-1 border-amber-200">
+    <div className="max-w-screen-md mx-auto mt-16 px-4 p-3 relative custom-card rounded-2xl ">
       {children}
     </div>
   );
