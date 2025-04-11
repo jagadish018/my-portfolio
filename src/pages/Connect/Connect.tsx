5d10ad111edb13fba1922e2ef4828021b1ba0389
jagadishck018 <jagadishck.018@gmail.com>
2025-04-11 16:00:07 +0530

    fix: update link in Connect component to direct to email compose instead of LinkedIn

--- src/pages/Connect/Connect.tsx
+++ src/pages/Connect/Connect.tsx
@@ -12,7 +12,7 @@ const Connect = () => {
         <div className="flex flex-col gap-4 items-center">
           <CurrentTimeIST /> 
           <a
-            href="https://www.linkedin.com/in/jagadish-c-k-93b729187/"
+            href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=new"
             target="_blank"
             rel="noopener noreferrer"
             className="text-amber-50 hover:text-amber-100 text-center bg-blue-500 rounded-3xl cursor-pointer p-2 w-36"
